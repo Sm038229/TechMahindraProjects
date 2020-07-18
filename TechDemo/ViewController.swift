0df4ef01463031c1e2187668563a488814f4c361
Sachin Mishra <sachchida@netsutra.com>
2020-07-18 14:14:03 +0530

    Initial Commit

---
+++ TechDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechDemo
+//
+//  Created by SachchidaNand Mishra on 18/07/20.
+//  Copyright © 2020 Sachchida Nand Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
